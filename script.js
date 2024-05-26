f4e727f4ab42d8f043cc086e0462c5eb86053eff
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-05-26 08:33:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSideBar(){
-    const sideBar = document.querySelector(".sidebar");
-    sideBar.style.display = 'flex';
-}
-
-function hideSideBar(){
-    const sideBar = document.querySelector(".sidebar");
-    sideBar.style.display = 'none';
-}
